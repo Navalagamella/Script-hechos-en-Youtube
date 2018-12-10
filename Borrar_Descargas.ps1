4be05cdeb5d9e28c180bd249b444db9bbf5b43dd
benjaminmalama <benjaminmalama@gmail.com>
2018-12-10 15:07:04 +0100

    Guía Básica #1 Remove-Item New-Item

---
+++ Borrar_Descargas.ps1
@@ -0,0 +1,22 @@
+﻿#Borrar Datos Descargados.
+
+#Borrar Carpeta Descarga.
+
+Remove-Item -Path Z:\Chrome -Recurse -Confirm:$false
+
+#Creacion de carpetas
+
+New-Item -Path Z:\ -Name Chrome -ItemType Directory
+
+New-Item -Path Z:\Chrome -Name Imagenes -ItemType Directory
+    New-Item -Path Z:\Chrome\Imagenes -Name WhatsApp -ItemType Directory
+
+New-Item -Path Z:\Chrome -Name "FP -Rodrigo Caro" -ItemType Directory
+    New-Item -Path 'Z:\Chrome\FP -Rodrigo Caro' -Name Descargas -ItemType Directory
+    New-Item -Path 'Z:\Chrome\FP -Rodrigo Caro' -Name Ejercicios -ItemType Directory
+    New-Item -Path 'Z:\Chrome\FP -Rodrigo Caro' -Name Mis_Proyectos -ItemType Directory
+    New-Item -Path 'Z:\Chrome\FP -Rodrigo Caro' -Name Tareas -ItemType Directory
+
+New-Item -Path Z:\Chrome -Name Documentos -ItemType Directory
+
+New-Item -Path Z:\Chrome -Name Aplicaciones -ItemType Directory
